13780a4dfc7fedab8d19d109844757e432b85bef
Chris Shim <seungyongshim@users.noreply.github.com>
2021-07-26 13:05:45 +0900

    Update setup.sql

--- infrastructure/mssql/setup.sql
+++ infrastructure/mssql/setup.sql
@@ -9,3 +9,37 @@ GRANT CREATE TABLE TO MT;
 GRANT ALTER, SELECT, INSERT ON SCHEMA::dbo TO MT;
 
 GO
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [IpInfo] (
+    [Id] int  NOT NULL ,
+    [IpAddress] varchar(16)  NOT NULL ,
+    [SendGrantId] int  NOT NULL ,
+    CONSTRAINT [PK_IpInfo] PRIMARY KEY CLUSTERED (
+        [Id] ASC
+    )
+)
+
+CREATE TABLE [SendGrant] (
+    [Id] int  NOT NULL ,
+    [Name] varchar(10)  NOT NULL ,
+    CONSTRAINT [PK_SendGrant] PRIMARY KEY CLUSTERED (
+        [Id] ASC
+    )
+)
+
+ALTER TABLE [IpInfo] WITH CHECK ADD CONSTRAINT [FK_IpInfo_SendGrantId] FOREIGN KEY([SendGrantId])
+REFERENCES [SendGrant] ([Id])
+
+ALTER TABLE [IpInfo] CHECK CONSTRAINT [FK_IpInfo_SendGrantId]
+
+CREATE INDEX [idx_IpInfo_IpAddress]
+ON [IpInfo] ([IpAddress])
+
+COMMIT TRANSACTION QUICKDBD
+
+GO
